a68e21ef4001c949aa7d32a87a3993bbd8525fa3
Jeremy D. Lea <reg@openpave.org>
2013-11-23 07:08:31 +0000

    Add a calculator page.  Beginnings of a web based calculator...

---
+++ calculate.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ *  calculate.php - About OpenPave.org
+ *
+ *  $OpenPave: www/calculate.php,v 1.1 2013/11/23 07:08:31 reg Exp $
+ *
+ *  The contents of this file are subject to the Academic Development
+ *  and Distribution License Version 1.0 (the "License"); you may not
+ *  use this file except in compliance with the License.  You should
+ *  have received a copy of the License with this file.  If you did not
+ *  then please contact whoever distributed this file too you, since
+ *  they may be in violation of the License, and this may affect your
+ *  rights under the License.
+ *
+ *  Software distributed under the License is distributed on an "AS IS"
+ *  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ *  the License for the specific language governing rights and
+ *  limitations under the License.
+ *
+ *  The Initial Developer of the Original Software is Jeremy Lea.
+ *
+ *  Portions Copyright (C) 2006-2009 OpenPave.org.
+ *
+ *  Contributor(s): Jeremy Lea <reg@openpave.org>.
+ */
+
+$depth = '';
+require($depth.'dynamic.inc');
+StartHTML(array(),array('//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js'));
+?>
+
+<h2>Calculate!</h2>
+
+<p>Work in progress...</p>
+
+<?php
+FinaliseHTML('$OpenPave: www/calculate.php,v 1.1 2013/11/23 07:08:31 reg Exp $');
+?>
